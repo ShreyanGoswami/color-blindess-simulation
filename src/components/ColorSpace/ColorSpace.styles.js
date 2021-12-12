80ef5d0c8e3fb44ddff4f15622f8e4f0d467cdc8
ShreyanGoswami <overclockedgtx@gmail.com>
2021-12-12 13:05:41 -0500

    Responsive design

--- src/components/ColorSpace/ColorSpace.styles.js
+++ src/components/ColorSpace/ColorSpace.styles.js
@@ -3,4 +3,13 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
 `
 
-export const Content = styled.div``;
+export const Content = styled.div`
+
+    word-wrap:break-word;
+
+    .math-equation {
+        @media screen and (max-width:768px) {
+            font-size:0.7em;
+        }
+    }
+`;
--- src/components/ColorSpace/index.js
+++ src/components/ColorSpace/index.js
@@ -14,7 +14,7 @@ const ColorSpace = ({ title, data, callback, mathConfig }) => {
     const b = createRef(null);
 
     for (let i = 0; i < data.length; i++) {
-        textToBeDisplayed.push(<p key={i}>{data[i]}</p>);
+        textToBeDisplayed.push(<div key={i} className="col-sm6">{data[i]}</div>);
     }
 
     const linearize = () => {
@@ -24,7 +24,6 @@ const ColorSpace = ({ title, data, callback, mathConfig }) => {
         temp.push(removeGamma(b.current.value));
         setState(temp);
         if (callback !== undefined) {
-            console.log("Calling");
             callback(temp);
         }
     };
@@ -38,26 +37,28 @@ const ColorSpace = ({ title, data, callback, mathConfig }) => {
                     <p className="additional-info">For a given pixel value, <span>$c$</span> between 0 and 1</p>
                     <span>{`$$ f(c) = \\begin{cases} \\frac{\\frac{c}{255}}{12.92}, & \\text{c<=0.04045 * 255} \\\\ \\frac{\\frac{c}{255}+0.055}{1.055}, & \\text{c>0.04045 * 255} \\end{cases} $$`}</span>
                 </MathJax>
-                <p>You might be aware that a pixel value is between 0 and 255. So first you have to scale the value down by dividing the pixel value by 255. Enter RGB values in the boxes below to see how the computation happens. Note: This step is required for images on disk which are converted into non linear RGB to improve the color quality</p>
+                <div className="">You might be aware that a pixel value is between 0 and 255. So first you have to scale the value down by dividing the pixel value by 255. Enter RGB values in the boxes below to see how the computation happens. Note: This step is required for images on disk which are converted into non linear RGB to improve the color quality</div>
                 <div className="input-group input-group-sm text-center">
-                    <InputGroup className="col col-lg-2">
+                    <InputGroup className="col col-sm-3">
                         <InputGroup.Text id="basic-addon1">R</InputGroup.Text>
                         <FormControl key="red"
                             placeholder={state[0]} ref={r} />
                     </InputGroup>
-                    <InputGroup className="col col-lg-2">
+                    <InputGroup className="col col-sm-3">
                         <InputGroup.Text id="basic-addon1">G</InputGroup.Text>
                         <FormControl key="green" placeholder={state[1]} ref={g} />
                     </InputGroup>
-                    <InputGroup className="col col-lg-2">
+                    <InputGroup className="col col-sm-3">
                         <InputGroup.Text id="basic-addon1">B</InputGroup.Text>
                         <FormControl key="blue" placeholder={state[2]} ref={b} />
                     </InputGroup>
-                    <Button onClick={linearize}>Calculate</Button>
+                    <Button className="col col-sm-2" onClick={linearize}>Calculate</Button>
                 </div>
+                <div className="math-equation">
                 <MathJax inline dynamic>
-                    <span>{`$$ (${state[0]}) \\hat{i} + (${state[1]}) \\hat{j} + (${state[2]}) \\hat{k} $$`}</span>
+                    <div className="col col-md-3">{`$$ (${state[0]}) \\hat{i} + (${state[1]}) \\hat{j} + (${state[2]}) \\hat{k} $$`}</div>
                 </MathJax>
+                </div>
             </MathJaxContext>
         </Content>
     </Wrapper>)
