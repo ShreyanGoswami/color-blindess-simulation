3c5904fe1fae14b55b8bc8a46ea1bf9c1634362f
ShreyanGoswami <overclockedgtx@gmail.com>
2021-12-28 16:52:33 -0500

    Fixed performance issue while graphing dynamically

--- src/compute/ColorSpace.js
+++ src/compute/ColorSpace.js
@@ -4,30 +4,41 @@ import { all, create } from "mathjs"
 const R_OFFSET = 0;
 const G_OFFSET = 1;
 const B_OFFSET = 2;
+const SAMPLING_SIZE = 3000;
 
 const config = {}
 const math = create(all, config)
 
-export const convertRGBToLMS = (imgData, h, w) => {
+export const convertRGBToLMS = (imgData) => {
     const RGB2LMS = [[0.31399022, 0.63951294, 0.04649755], [0.15537241, 0.75789446, 0.08670142], [0.01775239, 0.10944209, 0.87256922]]
     const workingCopy = Array.from(imgData)
 
+    const L = []
+    const M = []
+    const S = []
+
     // Converts RGB to LMS
-    for (let i = 0; i < h; i++) {
-        for (let j = 0; j < w; j++) {
-            const locationInArray = getIndex(j, i, w)
-            const r = removeGamma(workingCopy[locationInArray + R_OFFSET])
-            const g = removeGamma(workingCopy[locationInArray + G_OFFSET])
-            const b = removeGamma(workingCopy[locationInArray + B_OFFSET])
-            const l = RGB2LMS[0][0] * r + RGB2LMS[0][1] * g + RGB2LMS[0][2] * b
-            const m = RGB2LMS[1][0] * r + RGB2LMS[1][1] * g + RGB2LMS[1][2] * b
-            const s = RGB2LMS[2][0] * r + RGB2LMS[2][1] * g + RGB2LMS[2][2] * b
-            workingCopy[locationInArray + R_OFFSET] = l
-            workingCopy[locationInArray + G_OFFSET] = m
-            workingCopy[locationInArray + B_OFFSET] = s
+    const l = workingCopy.length;
+    let i = 0;
+    while (i < l) {
+        const locationInArray = i;
+        const r = removeGamma(workingCopy[locationInArray + R_OFFSET]);
+        const g = removeGamma(workingCopy[locationInArray + G_OFFSET]);
+        const b = removeGamma(workingCopy[locationInArray + B_OFFSET]);
+        const l = RGB2LMS[0][0] * r + RGB2LMS[0][1] * g + RGB2LMS[0][2] * b;
+        const m = RGB2LMS[1][0] * r + RGB2LMS[1][1] * g + RGB2LMS[1][2] * b;
+        const s = RGB2LMS[2][0] * r + RGB2LMS[2][1] * g + RGB2LMS[2][2] * b;
+        workingCopy[locationInArray + R_OFFSET] = l;
+        workingCopy[locationInArray + G_OFFSET] = m;
+        workingCopy[locationInArray + B_OFFSET] = s;
+        if (i % SAMPLING_SIZE === 0) {
+            L.push(l);
+            M.push(m);
+            S.push(s);
         }
+        i += 4;
     }
-    return workingCopy
+    return [workingCopy, L, M, S]
 }
 
 /**
@@ -126,20 +137,28 @@ export const simulateColorBlindness = (imgData, h, w, normal) => {
 
 export const simulateProtanopia = (imgData, h, w, plane1, plane2, neutralWhite) => {
     const workingCopy = Array.from(imgData)
-    for (let i = 0; i < h; i++) {
-        for (let j = 0; j < w; j++) {
-            const locationInArray = getIndex(j, i, w)
-            const l = workingCopy[locationInArray + R_OFFSET];
-            const m = workingCopy[locationInArray + G_OFFSET];
-            const s = workingCopy[locationInArray + B_OFFSET];
-            const color = [l,m,s];
-            const res = projectColorOnNormalForProtanopia(plane1, plane2, neutralWhite, color);
-            workingCopy[locationInArray + R_OFFSET] = res[0]
-            workingCopy[locationInArray + G_OFFSET] = res[1]
-            workingCopy[locationInArray + B_OFFSET] = res[2]
+
+    const L = [];
+    const M = [];
+    const S = [];
+    const l = workingCopy.length;
+    let i = 0;
+    while (i < l) {
+        const l = workingCopy[i + R_OFFSET];
+        const m = workingCopy[i + G_OFFSET];
+        const s = workingCopy[i + B_OFFSET];
+        const res = projectColorOnNormalForProtanopia(plane1, plane2, neutralWhite, [l,m,s]);
+        workingCopy[i + R_OFFSET] = res[0]
+        workingCopy[i + G_OFFSET] = res[1]
+        workingCopy[i + B_OFFSET] = res[2]
+        if (i % SAMPLING_SIZE === 0) {
+            L.push(res[0]);
+            M.push(res[1]);
+            S.push(res[2]);
         }
+        i += 4;
     }
-    return workingCopy
+    return [workingCopy, L, M, S];
 }
 
 const getIndex = (x, y, w) => {
