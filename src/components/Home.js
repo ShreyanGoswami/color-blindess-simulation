c7e3f115dec9a659d20389fa22a36c5330b5d0c6
ShreyanGoswami <overclockedgtx@gmail.com>
2022-01-01 20:00:12 -0500

    Removed unused imports and added new component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,13 @@ import Header from "./Header";
 import { Wrapper } from "./Home.styles";
 import Info from "./Info";
 import Normal from "./Normal";
-import LMS from "./LMS";
 import Protanopia from "./Protanopia";
 
 import data from "../assets/data";
 import Simulation from "./Simulation";
 
 import test from "./../assets/test1.jpg"
+import Intuition from "./Intuition";
 
 const Home = () => {
 
@@ -43,14 +43,13 @@ const Home = () => {
     const LMS2 = [0.9847601, 0.87614013, 0.00165276]
     const neutralWhite = [1.027,0.9847,0.9182]
 
-    const pixel = [0,0,0];
-
     return (<Wrapper className="container">
         <Header></Header>
         <Info title={data[getId(1, data)][title]} data={data[getId(1, data)][text]}></Info>
         <Info title={data[getId(2, data)][title]} data={data[getId(2, data)][text]}></Info>
         <Info title={data[getId(3, data)][title]} data={data[getId(3, data)][text]}></Info>
         <Info title={data[getId(4, data)][title]} data={data[getId(4, data)][text]}></Info>
+        <Intuition></Intuition>
         <Normal title={data[getId(5, data)][title]} data={data[getId(5, data)][text]} mathConfig={mathJaxConfig} lms1={LMS1} lms2={LMS2} white={neutralWhite}></Normal>
         <Simulation title={data[getId(6, data)][title]} data={data[getId(6, data)][text]} invariant1={LMS1} invariant2={LMS2} white={neutralWhite} config={mathJaxConfig}></Simulation>
         <Protanopia title={data[getId(7, data)][title]} data={data[getId(7, data)][text]} invariant1={LMS1} invariant2={LMS2} white={neutralWhite} imageForSimulation={test} config={mathJaxConfig}></Protanopia>
---
+++ src/components/Intuition/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Wrapper, Content } from "./Intuition.styles";
+
+const Intuition = () => {
+
+    return (
+        <Wrapper>
+            <Content>
+                
+            </Content>
+        </Wrapper>
+    )
+
+}
+
+export default Intuition;
