63a78b823ba59fbb4726bd9b4bdb1c595117eace
ShreyanGoswami <overclockedgtx@gmail.com>
2022-01-02 20:11:01 -0500

    Updated logic for plot

--- src/components/Locus/index.js
+++ src/components/Locus/index.js
@@ -6,11 +6,18 @@ import { Wrapper, Content } from "./Locus.styles";
 import xData from '../../assets/lData';
 import yData from '../../assets/mData';
 import zData from '../../assets/sData';
+import { configFor3DMesh } from "../../utils/graphPlotHelper";
+
 import { useEffect } from "react";
+import { FormCheck } from "react-bootstrap";
 
 const Locus = () => {
 
     useEffect(() => {
+       plotLocus();
+    }, []);
+
+    const plotLocus = () => {
         const trace = {
             x: xData,
             y: yData,
@@ -18,19 +25,55 @@ const Locus = () => {
             type: "scatter3d",
             mode: "lines+markers",
             line: {
-                width: 6,
+                width: 3,
                 color: 'rgb(127,127,127)'
+            },
+            marker: {
+                size: 5
             }
         }
-    
         const data = [trace]
         Plotly.newPlot("visualize-locus", data);
-    },[])
+    }
+
+    const handleProjectionPlaneEnabled = (e) => {
+        if (e.target.checked) {
+            const trace = {
+                x: xData,
+                y: yData,
+                z: zData,
+                type: "scatter3d",
+                mode: "lines+markers",
+                line: {
+                    width: 6,
+                    color: 'rgb(127,127,127)'
+                }
+            }
+
+            const planeData = {
+                type: "mesh3d",
+                x: [0.05235866,0.9847601,1.027,0],
+                y: [0.14667038,0.87614013,0.9847,0],
+                z: [0.95667258,0.00165276,0.9182,0],
+                i: [0,1],
+                j: [2,2],
+                k: [3,3]
+            }
+            const data = [trace, planeData];
+            Plotly.newPlot("visualize-locus", data);
+        } else {
+            plotLocus();
+        }
+    }
 
     return (
         <Wrapper>
             <Content>
-
+                <FormCheck 
+                type="switch"
+                label="Show projection plane"
+                onChange={(e)=>handleProjectionPlaneEnabled(e)}
+                />
                 <div id="visualize-locus"></div>
             </Content>
         </Wrapper>
