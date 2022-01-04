2c7af5e7350a36e14e8e430bca685982dd599b79
ShreyanGoswami <overclockedgtx@gmail.com>
2022-01-03 19:02:32 -0500

    Prevent graph re rendering

--- src/components/Locus/index.js
+++ src/components/Locus/index.js
@@ -1,112 +1,167 @@
-import React from "react";
-import * as Plotly from 'plotly.js';
+import React, {useEffect} from "react";
 
 import { Wrapper, Content } from "./Locus.styles";
+
+import { FormCheck } from "react-bootstrap";
+
+import * as Plotly from 'plotly.js';
 
 import xData from '../../assets/lData';
 import yData from '../../assets/mData';
 import zData from '../../assets/sData';
-import { configFor3DMesh } from "../../utils/graphPlotHelper";
 
-import { useEffect } from "react";
-import { FormCheck } from "react-bootstrap";
+import pxData from "../../assets/pxData";
+import pyData from "../../assets/pyData";
+import pzData from "../../assets/pzData";
 
 const Locus = () => {
 
-    useEffect(() => {
-       plotLocus();
-    }, []);
-
-    const plotLocus = () => {
-        const trace = {
-            x: xData,
-            y: yData,
-            z: zData,
-            type: "scatter3d",
-            mode: "lines+markers",
-            line: {
-                width: 3,
-                color: 'rgb(127,127,127)'
-            },
-            marker: {
-                size: 5
-            }
-        }
-        const layout = {
-            height: 500,
-            paper_bgcolor: '#f8f9fa'
-        }
-        const data = [trace]
-        Plotly.newPlot("visualize-locus", data, layout);
-    }
-
     const handleProjectionPlaneEnabled = (e) => {
-        if (e.target.checked) {
-            const trace = {
-                x: xData,
-                y: yData,
-                z: zData,
-                type: "scatter3d",
-                mode: "lines+markers",
-                line: {
-                    width: 6,
-                    color: 'rgb(127,127,127)'
-                }
-            }
-
-            const plane1 = {
-                type: "mesh3d",
-                x: [0.05235866,1.027,0],
-                y: [0.14667038,0.9847,0],
-                z: [0.95667258,0.9182,0],
-                color:'rgb(233,207,187)',
-                i: [0],
-                j: [1],
-                k: [2]
-            }
-            const plane2 = {
-                type: "mesh3d",
-                x: [0.9847601,1.027,0],
-                y: [0.87614013,0.9847,0],
-                z: [0.00165276,0.9182,0],
-                color:'rgb(198,188,222)',
-                opacity:0.75,
-                i: [0],
-                j: [1],
-                k: [2]
-            }
-            const layout = {
-                height: 500,
-                paper_bgcolor: '#f8f9fa'
-            }
-            const data = [trace, plane1, plane2];
-            Plotly.newPlot("visualize-locus", data, layout);
-        } else {
-            plotLocus();
-        }
+        const update = {visible: e.target.checked};
+        Plotly.restyle("plot", update, [1,2]);
     }
+
+    const displayProtanopiaLocus = (e) => {
+        
+    }
+
+    const layout = {
+        height: 800,
+        name: 'Spectral locus',
+        showlegend: true,
+        legend: {
+            x: 1,
+            xanchor: 'right',
+            y: 0.9,
+        },
+        margin: {
+            l: 0,
+            r: 0,
+            b: 0,
+            t: 0
+        },
+        uirevision: true,
+        paper_bgcolor: 'rgba(0, 0, 0, 0)',
+        scene: {
+            camera: {
+                projection: {
+                    type: 'orthographic'
+                }
+            },
+            // https://plotly.com/javascript/3d-axes/
+            //aspectmode: 'cube',
+            xaxis: {
+                autorange: true,
+                //range: [0, 1],
+                zeroline: true,
+                zerolinecolor: '#000000',
+                zerolinewidth: 5,
+                showspikes: false,
+                title: {
+                    text: 'L'
+                }
+            },
+            yaxis: {
+                autorange: true,
+                zeroline: true,
+                zerolinecolor: '#000000',
+                zerolinewidth: 5,
+                scaleanchor: 'x',
+                showspikes: false,
+                title: {
+                    text: 'M'
+                }
+            },
+            zaxis: {
+                autorange: true,
+                zeroline: true,
+                zerolinecolor: '#000000',
+                zerolinewidth: 5,
+                showspikes: false,
+                title: {
+                    text: 'S'
+                }
+            },
+        }
+    };
+
+    const lmsTrace = {
+        x: xData,
+        y: yData,
+        z: zData,
+        type: "scatter3d",
+        mode: "lines+markers",
+        line: {
+            width: 2,
+            color: '#000000'
+        },
+        marker: {
+            size: 6,
+            opacity: 1
+        },
+        visible:true
+    }
+
+    const protanopiaLocus = {
+        x: pxData,
+        y: pyData,
+        z: pzData
+    }
+
+    const plane1 = {
+        type: "mesh3d",
+        x: [0.05235866, 1.027, 0],
+        y: [0.14667038, 0.9847, 0],
+        z: [0.95667258, 0.9182, 0],
+        color: 'purpleColor',
+        opacity: 0.3,
+        i: [0],
+        j: [1],
+        k: [2],
+        visible: false
+    }
+    const plane2 = {
+        type: "mesh3d",
+        x: [0.9847601, 1.027, 0],
+        y: [0.87614013, 0.9847, 0],
+        z: [0.00165276, 0.9182, 0],
+        color: 'orangeColor',
+        opacity: 0.3,
+        i: [0],
+        j: [1],
+        k: [2],
+        visible:false
+    }
+
+    const data = [lmsTrace, plane1, plane2, protanopiaLocus];
+
+    useEffect(() => {
+        Plotly.newPlot("plot", data, layout);
+        console.log('Drawing');
+    });
 
     return (
         <Wrapper>
             <Content>
                 <div className="controls">
-                <FormCheck 
-                type="switch"
-                label="Show projection plane"
-                onChange={(e)=>handleProjectionPlaneEnabled(e)}
-                />
-                <FormCheck 
-                type="switch"
-                label="Show Protanopia Locus"
-                onChange={(e)=>handleProjectionPlaneEnabled(e)}
-                />
-                <FormCheck 
-                type="switch"
-                label="Show sRGB Gamut"
-                onChange={(e)=>handleProjectionPlaneEnabled(e)}
-                />
+                    <FormCheck
+                        type="switch"
+                        label="Show projection plane"
+                        onChange={(e) => handleProjectionPlaneEnabled(e)}
+                    />
+                    <FormCheck
+                        type="switch"
+                        label="Show Protanopia Locus"
+                        onChange={(e) => displayProtanopiaLocus(e)}
+                    />
+                    <FormCheck
+                        type="switch"
+                        label="Show sRGB Gamut"
+                        onChange={(e) => handleProjectionPlaneEnabled(e)}
+                    />
                 </div>
-                <div id="visualize-locus" className="col-lg-9 col-sm-5 long"></div>
+                {/* <LocusVisualizer isPlaneVisible={plane[i]} isProtanopeLocusVisible={false} isSRGBGamutVisible={false}></LocusVisualizer> */}
+                <div id="plot"></div>
             </Content>
         </Wrapper>
     )
